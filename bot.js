bb6c7d435bbad70b81821ed9facaec657ee66149
MahmutX21 <kutluaymahmut21@gmail.com>
2026-02-25 20:23:15 +0300

    Updatebot.js

--- bot.js
+++ bot.js
@@ -1,3 +1,4 @@
+const { ChannelType } = require('discord.js');
 const { Client, GatewayIntentBits, SlashCommandBuilder } = require('discord.js');
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v10');
